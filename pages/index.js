668dcd525d5048c5897ab1f308f2d245a3b5e3b8
Lucas Mondadori <lmondadori@gmail.com>
2024-02-16 10:50:56 -0300

    New host 3

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
 
       <main>
         <Header title="Comunicado sobre alteração de domínio" />
-        <p className="description" style="font-size: 20px; text-align: center;">
+        <p className="description" style={{ fontSize: '20px', textAlign: 'center' }}>
           A Horus Aeronaves comunica a todos que, a partir do dia 19 de fevereiro de 2024, a plataforma do sistema Monitora Floripa terá alteração no domínio. <br></br><br></br> 
           Isso significa que para abrir o nosso site no seu computador ou celular, você deve entrar no endereço abaixo: <br></br><br></br>  
           <a href='https://pmf.horus.global/'>https://pmf.horus.global/</a>.
